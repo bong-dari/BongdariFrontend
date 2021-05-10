900765afacf42e889f74764a09ec4e09457e4560
dongchan-K <ehdcks1028@gmail.com>
2021-05-10 14:14:23 +0900

    fix: 기관 양식 수정

--- App.js
+++ App.js
@@ -10,7 +10,6 @@ import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
 import { StatusBar } from 'react-native';
 import MainTabStackScreen from './src/screens/MainTabStackScreen';
-import InstitutionAddressForm from './src/screens/institution/InstitutionAddressForm';
 
 const App = () => {
   return (
