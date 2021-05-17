e8dc8baeb163d62b98d6450c410110229e36464e
d0ngt0k <ehdcks1028@naver.com>
2021-05-17 22:44:21 +0900

    feat: 로그인 양식 레이아웃 수정

--- src/screens/institution/InstitutionLogin.js
+++ src/screens/institution/InstitutionLogin.js
@@ -8,6 +8,8 @@ import {
   SafeAreaView,
   Image,
   TextInput,
+  KeyboardAvoidingView,
+  Platform,
 } from 'react-native';
 
 const InstitutionLogin = () => {
@@ -18,35 +20,39 @@ const InstitutionLogin = () => {
   return (
     <SafeAreaView style={styles.wrapper}>
       <View style={styles.topCircle} />
-      <View style={styles.rightCircle} />
-      <View style={styles.bottomCircle} />
-      <View style={styles.viewContainer}>
+      <KeyboardAvoidingView
+        style={styles.viewContainer}
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+        <View style={styles.rightCircle} />
+        <View style={styles.bottomCircle} />
         <Text style={styles.appName}>Bongdari</Text>
-        <Text style={styles.comment}>로그인</Text>
-        <View style={styles.divisionLine} />
+        <View style={styles.divisionContainer}>
+          <View style={styles.divisionLine} />
+          <Text style={styles.divisionText}>로그인</Text>
+        </View>
         <TextInput
           style={styles.loginInput}
           placeholder="아이디"
-          placeholderTextColor="#ededed"
+          placeholderTextColor="#c4c4c4"
         />
         <TextInput
           style={styles.loginInput}
           placeholder="비밀번호"
-          placeholderTextColor="#ededed"
+          placeholderTextColor="#c4c4c4"
         />
         <TouchableOpacity style={styles.loginButton}>
           <Text style={styles.loginText}>로그인</Text>
         </TouchableOpacity>
-        <Text style={styles.find}>아이디 찾기 &#47; 비밀번호 찾기</Text>
-        <Text>
+        <Text style={styles.findLink}>아이디 찾기 &#47; 비밀번호 찾기</Text>
+        <Text style={{ color: '#000' }}>
           계정이 없으신가요?{' '}
           <Text
-            style={styles.register}
+            style={styles.registerLink}
             onPress={() => onPress({ type: 'InstitutionIdForm' })}>
             기관등록
           </Text>
         </Text>
-      </View>
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
 };
@@ -77,40 +83,6 @@ const styles = StyleSheet.create({
     shadowRadius: 8,
     elevation: 6,
   },
-  rightCircle: {
-    position: 'absolute',
-    top: '14%',
-    right: '-5%',
-    width: 63,
-    height: 63,
-    backgroundColor: '#2de2ac',
-    borderRadius: 50,
-    shadowColor: '#000',
-    shadowOpacity: 0.25,
-    shadowOffset: {
-      width: 4,
-      height: 20,
-    },
-    shadowRadius: 8,
-    elevation: 4,
-  },
-  bottomCircle: {
-    position: 'absolute',
-    bottom: '13%',
-    left: '15%',
-    width: 84,
-    height: 84,
-    backgroundColor: '#ffb623',
-    borderRadius: 50,
-    shadowColor: '#000',
-    shadowOpacity: 0.25,
-    shadowOffset: {
-      width: 4,
-      height: 4,
-    },
-    shadowRadius: 8,
-    elevation: 6,
-  },
   viewContainer: {
     width: '100%',
     height: '60%',
@@ -127,27 +99,76 @@ const styles = StyleSheet.create({
     shadowRadius: 4,
     elevation: 5,
   },
+  rightCircle: {
+    position: 'absolute',
+    bottom: '95%',
+    right: '-12%',
+    width: 63,
+    height: 63,
+    backgroundColor: '#2de2ac',
+    borderRadius: 50,
+    shadowColor: '#000',
+    shadowOpacity: 0.25,
+    shadowOffset: {
+      width: 4,
+      height: 20,
+    },
+    shadowRadius: 8,
+    elevation: 4,
+  },
+  bottomCircle: {
+    position: 'absolute',
+    bottom: '-10%',
+    left: '15%',
+    width: 58,
+    height: 58,
+    backgroundColor: '#ffb623',
+    borderRadius: 50,
+    shadowColor: '#000',
+    shadowOpacity: 0.25,
+    shadowOffset: {
+      width: 4,
+      height: 4,
+    },
+    shadowRadius: 8,
+    elevation: 6,
+  },
   appName: {
     color: '#3eb1c8',
     fontSize: 20,
     fontWeight: 'bold',
   },
-  comment: {
-    color: '#000000',
-    fontSize: 20,
-    marginTop: 34,
+  divisionContainer: {
+    width: '100%',
+    height: 20,
+    position: 'relative',
+    marginTop: 60,
+    marginBottom: 10,
+    justifyContent: 'center',
   },
   divisionLine: {
     width: '100%',
     height: 1,
-    marginTop: 10,
-    backgroundColor: '#ededed',
+    backgroundColor: '#929292',
+  },
+  divisionText: {
+    color: '#8c8c8c',
+    backgroundColor: '#fff',
+    textAlign: 'center',
+    position: 'absolute',
+    width: 80,
+    left: '50%',
+    top: '50%',
+    marginLeft: -40,
+    marginTop: -10,
+    fontSize: 15,
   },
   loginInput: {
     marginTop: 10,
+    paddingLeft: 7,
     width: '100%',
     height: 40,
-    borderColor: '#ededed',
+    borderColor: '#c4c4c4',
     borderWidth: 1,
     borderRadius: 5,
     fontSize: 16,
@@ -165,11 +186,11 @@ const styles = StyleSheet.create({
     color: '#ffffff',
     fontSize: 16,
   },
-  find: {
+  findLink: {
     fontSize: 14,
     color: '#008489',
   },
-  register: {
+  registerLink: {
     fontSize: 14,
     color: '#008489',
     textDecorationLine: 'underline',
